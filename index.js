e21072953d87b92a6d68bf912737e35a50ec75f6
wilmer <wilmerandres505@gmail.com>
2020-11-12 20:48:53 -0500

    checar si es palabra es palindromo o no

---
+++ index.js
@@ -0,0 +1,16 @@
+﻿/*
+El metacarácter \ W se utiliza para encontrar 
+un carácter que no sea una palabra. Un carácter 
+de palabra es un carácter de az, AZ, 0-9, incluido 
+el carácter de subrayado . Esto significa que si usa
+https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_Expressions
+Expresiones irregulares
+ [\ W] y no [\ W_]*/
+function palindrome(str) {
+    let varString = str.replace(/\W+|_/g, "").toLowerCase();
+    let reverseString = varString.split("").reverse().join("");
+    return varString === reverseString ? true : false;
+
+}
+let result=palindrome("anitalavalatina");
+alert(result);
